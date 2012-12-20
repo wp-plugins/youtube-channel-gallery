e42c3b0abf478d3a779a5285fd93d93ef4adc25f
javitxu123 <javitxu123@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-20 23:14:27 +0000

    = 1.7.4 =
    git-svn-id: https://plugins.svn.wordpress.org/youtube-channel-gallery/trunk@642652 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- youtube-channel-gallery.php
+++ youtube-channel-gallery.php
@@ -5,7 +5,7 @@
 	Description: Show a youtube video and a gallery of thumbnails for a youtube channel.
 	Author: Javier Gómez Pose
 	Author URI: http://www.poselab.com/
-	Version: 1.7.3
+	Version: 1.7.4
 	License: GPL2
 		
 		Copyright 2010 Javier Gómez Pose  (email : javierpose@gmail.com)
@@ -590,13 +590,13 @@
 							}
 							
 							//to appropriate thumbnail
+
+							//media:thumbnail tag
+							$media_group = $item->get_item_tags('http://search.yahoo.com/mrss/', 'group');
+							$media_content = $media_group[0]['child']['http://search.yahoo.com/mrss/']['thumbnail'];
+
+							//Check the thumbnail width
 							if($plugincount == 0){
-
-								//media:thumbnail tag
-								$media_group = $item->get_item_tags('http://search.yahoo.com/mrss/', 'group');
-								$media_content = $media_group[0]['child']['http://search.yahoo.com/mrss/']['thumbnail'];
-
-								//Check the thumbnail width
 								$thumbW = array();
 								foreach ($media_content as $index => $media_contentw) {
 									$thumbW[$index] = $media_content[$index]['attribs']['']['width'];
@@ -605,11 +605,11 @@
 								$thumbcorrectW = $this->closest($thumbW, $ytchag_thumb_width);
 
 								//index in array of thumbnail width
-	    						$thumbcorrectWIndex = array_search($thumbcorrectW, $thumbW);
+	    							$thumbcorrectWIndex = array_search($thumbcorrectW, $thumbW);
+    							}
 
 	    						//appropriate url thumbnail
 	    						$thumb = $media_content[$thumbcorrectWIndex]['attribs']['']['url'];
-    						}
 
 
 							//title and description content
